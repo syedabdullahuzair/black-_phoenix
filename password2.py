9daac6d355d40f24d7f59c6a26660762f2ca1f98
syedabdullahuzair <syedabdullahuzair13@gmail.com>
2026-09-13 22:11:20 +0500

    Add files via upload

---
+++ password2.py
@@ -0,0 +1,15 @@
+password=int(input("Enter you secret code:"))
+password_=int(input("Enter your secret code for access:"))
+#if else condition is apply to print the code or to check that the access is granted
+if password==password_:
+    print("Access to your account is granted!")#if access is granted this will be printed 
+else:
+    print("Acces to your account is not granted!")#if the access is not granted this will print beacause the entered secret password is wrong
+
+if password==password_:
+    print("enjoy youe account after the access you have granted")
+else:
+    print("dont enjoy youe account because your access is not grated beacuse of your lazzyness you fool")
+    
+    
+
