430d7e73ed7da989c5678336c3ba7dabd1db9c64
syedabdullahuzair <syedabdullahuzair13@gmail.com>
2026-09-15 22:02:41 +0500

    Add files via upload
    i made this program that it can be used to calculate the quantity of an item left in stock with using if elif else loop

---
+++ item calculator.py
@@ -0,0 +1,32 @@
+item1=int(input("Enter the quantity of first item:"))
+item2=int(input("Enter the quantity of second item:"))
+item3=int(input("Enter the quantity of third item:"))
+item4=int(input("Enter the quantity of fourth item:"))
+item5=int(input("Enter the quantity of fifth item:"))
+item6=int(input("Enter the quantity of sixth item:"))
+item7=int(input("Enter the quantity of seventh item:"))
+item8=int(input("Enter the quantity of eighth item:"))
+item9=int(input("Enter the quantity of ninth item:"))
+item10=int(input("Enter the quantity of tenth item:"))
+item11=(item1+item2+item3+item4+item5+item6+item7+item8+item9+item10)
+
+if item1==item2:
+    print(item1+item2)
+elif item2==item3:
+    print(item1+item2+item3)
+elif item3==item4:
+    print(item1+item2+item3+item4)
+elif item4==item5:
+    print(item1+item2+item3+item4+item5)
+elif item5==item6:
+    print(item1+item2+item3+item4+item5+item6)
+elif item6==item7:
+    print(item1+item2+item3+item4+item5+item6+item7)
+elif item7==item8:
+    print(item1+item2+item3+item4+item5+item6+item7+item8)
+elif item8==item9:
+    print(item1+item2+item3+item4+item5+item6+item8+item9+item10)
+else:
+    print(item11+item10)
+ 
+
