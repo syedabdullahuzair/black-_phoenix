112695048a38067fc4d848a8f1a3a608be7316da
syedabdullahuzair <syedabdullahuzair13@gmail.com>
2026-09-15 23:11:30 +0500

    Add files via upload

---
+++ pc price.py
@@ -0,0 +1,119 @@
+cpu=int(input("Enter the price of cpu:"))
+gpu=int(input("Enter teh price of gpu:"))
+ram=int(input("Enter the price of ram:"))
+psu=int(input("Enter the price of psu:"))
+monitor=int(input("Enter the price of monitor:"))
+mouse=int(input("Enter the price f mouse:"))
+keyboard=int(input("Enter the price of keyboard:"))
+mob=int(input("Enter the price of mother board:"))
+
+if cpu>=55000:
+    print("Too much expensive!!")
+elif cpu>=45000:
+    print("Too much expensive!!")
+elif cpu>=35000:
+    print("Too much expensive!!")
+elif cpu<=25000:
+    print("Reasonble price!!")
+else:
+    print("Buy at good price")
+
+
+if gpu>=100000:
+    print("Too much expensive")
+elif gpu>=90000:
+    print("Too much expensive")
+elif gpu>=80000:
+    print("Too much expensive")
+elif gpu<=70000:
+    print("At reasonable price")
+else:
+    print("Buy at reasonable price")
+
+
+
+if ram>=50000:
+    print("Too much expensive!")
+elif ram>=45000:
+    print("Too much expensive!")
+elif ram>=35000:
+    print("Too much expensive!")
+elif ram<=25000:
+    print("Buy at reasonable price!")
+else:
+    print("Buy it!at any cost!")
+
+
+
+if psu>=20000:
+    print("Too much expensive!")
+elif psu>=15000:
+    print("Too much expensive!")
+elif psu<=10000:
+    print("But it! at reasonable price!")
+else:
+    print("Buy at any cost")
+
+
+
+if monitor>=50000:
+    print("Too much expensive!")
+elif monitor>=40000:
+    print("Too much expensive!!")
+elif monitor<=30000:
+    print("Buy it t reasonable price!")
+else:
+    print("buy at nay cost!!")
+
+
+
+if mouse>=20000:
+    print("Too expensive!")
+elif mouse>=15000:
+    print("Too much expensive!")
+elif mouse<=10000:
+    print("But it at reasonable price!")
+
+
+
+if keyboard>=40000:
+    print("Too much expensive!!")
+elif keyboard>=30000:
+    print("Too muc expensive!!")
+elif keyboard<=25000:
+    print("But it at reasnable price!!")
+else:
+    print("Buy it at any cost")
+
+
+
+if mob>=70000:
+    print("too much expensive!")
+elif mob>=60000:
+    print("Toomuch expensive!!")
+elif mob>=50000:
+    print("Too much expensive")
+elif mob<=40000:
+    print("But it any any cost!!")
+
+TOTAL PRICE=cpu+gpu+ram+mouse+keyboard+psu+monitor+mob    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
